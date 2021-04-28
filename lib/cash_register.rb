4120ff9c94edae263814d2e8e6237b2996731e5d
Vince W <vincewallgren@me.com>
2021-04-28 02:26:59 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,5 +1,14 @@
  class CashRegister
 
+   attr_accessor :item, :last_item
+
+   def initialize(discount = 0)
+     @total = 0.0
+     @items = []
+     @discount = discount
+     
+   end
+
 
 
  end
