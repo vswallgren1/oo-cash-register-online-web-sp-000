83f57c44cafa6908eafa426bde33d8c184486440
Vince W <vincewallgren@me.com>
2021-04-28 02:34:00 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -6,9 +6,15 @@
      @total = 0.0
      @items = []
      @discount = discount
-     
+     @running_totals = [0.0]
    end
 
-
+   def add_item(title, price, quantity = 1)
+     @total = @total + price * quantity
+     @running_totals << total
+     for i in 1 .. quantity
+       @items << title
+     end
+   end
 
  end
